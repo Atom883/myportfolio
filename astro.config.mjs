6a29d7a2fc342f9e90dc6ae34bc88367633897a8
shoma883 <shouma.s.0509@gmail.com>
2025-07-22 16:10:44 +0900

    Add GitHub Pages deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Atom883.github.io', // ここ
+  base: '/myportfolio/',   // ここ
   vite: {
       plugins: [tailwindcss()],
   },
